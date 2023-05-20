f7535c031ffeaa4c8539cbfe84552657e5dd9cbd
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-21 00:36:39 +0600

    Updated views.py

--- App_main/views.py
+++ App_main/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics, viewsets, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from App_main.models import Profile, Post, Comment, Connection
+from App_main.models import Profile, Post, Comment, Connection, Like, Share
 from App_main.serializers import (
     ProfileSerializer,
     PostSerializer,
     CommentSerializer,
-    ConnectionSerializer,
+    ConnectionSerializer, LikeSerializer, ShareSerializer,
 )
 
 
@@ -37,3 +37,142 @@ class ProfileViewSet(viewsets.ModelViewSet):
         serializer.save()
         return Response(serializer.data)
 
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_author(self):
+        return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post = serializer.save(author=self.get_author())
+
+        return Response({"response": "successfully posted!"}, status=status.HTTP_201_CREATED)
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    lookup_field = 'id'
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        post = Post.objects.get(pk=pk)
+        return post
+
+    def get(self, request, *args, **kwargs):
+        post = self.get_object()
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+
+
+class CommentCreateView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_author(self):
+        return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+
+        comment = serializer.save(author=self.get_author())
+
+        return Response({"response": "comment posted"}, status=status.HTTP_201_CREATED)
+
+
+class CommentListView(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class LikeCreateView(generics.CreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_author(self):
+        return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+
+        like = serializer.save(author=self.get_author())
+
+        return Response({"response": "liked"}, status=status.HTTP_201_CREATED)
+
+
+class LikeListView(generics.ListAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ShareCreateView(generics.CreateAPIView):
+    queryset = Share.objects.all()
+    serializer_class = ShareSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_author(self):
+        return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+
+        share = serializer.save(author=self.get_author())
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ShareListView(generics.ListAPIView):
+    queryset = Share.objects.all()
+    serializer_class = ShareSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PostSearchView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.data.get('q')
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+        return queryset
+
+
+class ConnectionViewSet(viewsets.ModelViewSet):
+    queryset = Connection.objects.all()
+    serializer_class = ConnectionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(user=request.user)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
