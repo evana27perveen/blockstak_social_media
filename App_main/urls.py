cffe018df3f5e0e9436bbc9e5b5b75e4db10ca91
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-20 23:24:55 +0600

    Updated urls.py to perform the user profile's operation routings

--- App_main/urls.py
+++ App_main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 app_name = 'App_main'
 
 urlpatterns = [
-
+  path('profiles/', ProfileViewSet.as_view({'get': 'list', 'post': 'create'}), name='profile-list'),
+  path('profiles/my-operations/', ProfileViewSet.as_view({'get': 'retrieve', 'put': 'update'}), name='profile-detail'),
 ]
