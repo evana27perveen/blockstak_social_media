5b4d0cbb81b8513d6d304d6e6a4e9437a5601449
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-21 00:34:58 +0600

    Added the Profile Model, Post Model, Comment Model and Connection Model

--- App_main/models.py
+++ App_main/models.py
@@ -11,24 +11,52 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='post_author')
+    title = models.CharField(max_length=255)
     text = models.TextField()
     image = models.ImageField(upload_to='post_images')
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    likes = models.ManyToManyField(CustomUser, related_name='liked_posts')
-    comments = models.ManyToManyField(CustomUser, through='Comment', related_name='commented_posts')
-    shared_by = models.ManyToManyField(CustomUser, related_name='shared_posts')
+    @property
+    def likes_count(self):
+        return Like.objects.filter(post=self).count()
 
-    def get_comments(self):
-        return Comment.objects.filter(post=self)
+    @property
+    def comments_count(self):
+        return Comment.objects.filter(post=self).count()
+
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="comment_author")
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}: {self.text}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + " liked " + self.post.title
+
+
+class Share(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + " shared " + self.post.title
 
 
 class Connection(models.Model):
