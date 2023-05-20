4a3ccc6373897672c5cadd3bc97320f578874247
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-20 21:42:47 +0600

    Created CustomUser serializers.py

--- App_auth/serializers.py
+++ App_auth/serializers.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.models import Group
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import serializers
 from App_auth.models import CustomUser
@@ -16,15 +14,10 @@ class UserSerializers(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = CustomUser(
-            username=validated_data['username'],  # we can also write like this: validated_data.get('username')
+            username=validated_data['username'],  
         )
 
         user.set_password(validated_data['password'])
-        grp_name = self.context.get('group_name')
         user.save()
-        group = Group.objects.get_or_create(
-            name=grp_name
-        )  # group, created = Group.objects.get_or_create(name....)
-        group[0].user_set.add(user)
         return user
 
