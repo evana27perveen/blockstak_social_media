f5da383f669883d44565e319e19945264978a6c8
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-21 00:36:09 +0600

    Updated urls.py to perform the user profile's operation routings

--- App_main/urls.py
+++ App_main/urls.py
@@ -1,8 +1,26 @@
 from django.urls import path
+from .views import (
+    ProfileViewSet,
+    ConnectionViewSet, PostListView, PostCreateView, PostDetailView, CommentListView, CommentCreateView, LikeListView,
+    LikeCreateView, ShareListView, ShareCreateView, PostSearchView,
+)
 
 app_name = 'App_main'
 
 urlpatterns = [
-  path('profiles/', ProfileViewSet.as_view({'get': 'list', 'post': 'create'}), name='profile-list'),
-  path('profiles/my-operations/', ProfileViewSet.as_view({'get': 'retrieve', 'put': 'update'}), name='profile-detail'),
+    path('profiles/', ProfileViewSet.as_view({'get': 'list', 'post': 'create'}), name='profile-list'),
+    path('profiles/my-operations/', ProfileViewSet.as_view({'get': 'retrieve', 'put': 'update'}), name='profile-detail'),
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/create/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('comments/', CommentListView.as_view(), name='comment-list'),
+    path('comments/create/', CommentCreateView.as_view(), name='comment-create'),
+    path('likes/', LikeListView.as_view(), name='like-list'),
+    path('likes/create/', LikeCreateView.as_view(), name='like-create'),
+    path('shares/', ShareListView.as_view(), name='share-list'),
+    path('shares/create/', ShareCreateView.as_view(), name='share-create'),
+    path('posts/search/', PostSearchView.as_view(), name='post-search'),
+    path('connections/', ConnectionViewSet.as_view({'get': 'list', 'post': 'create'}), name='connection-list'),
+    path('connections/<int:pk>/', ConnectionViewSet.as_view({'get': 'retrieve'}), name='connection-detail'),
 ]
+
