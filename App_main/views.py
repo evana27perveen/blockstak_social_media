ac0614bba0fcb176c0e9a62a89e5890fbda05f2b
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-20 23:22:26 +0600

    Updated views.py to perform profile create, update, retrieve

--- App_main/views.py
+++ App_main/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets, status, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from App_main.models import Profile, Post, Comment, Connection
+from App_main.serializers import (
+    ProfileSerializer,
+    PostSerializer,
+    CommentSerializer,
+    ConnectionSerializer,
+)
 
-# Create your views here.
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = Profile.objects.get(user=request.user)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        print(request.user)
+        serializer = self.get_serializer(data=request.data, context={'user': request.user.id})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        instance = Profile.objects.get(user=request.user)
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
