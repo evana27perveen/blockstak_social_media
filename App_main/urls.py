fa2fdda4f531a54547f720b78e5d686f5c1fd143
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-22 23:05:35 +0600

    Update urls.py

--- App_main/urls.py
+++ App_main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     ProfileViewSet,
-    ConnectionViewSet, PostListView, PostCreateView, PostDetailView, CommentListView, CommentCreateView, LikeListView,
+    ConnectionListCreateView, PostListView, PostCreateView, PostDetailView, CommentListView, CommentCreateView, LikeListView,
     LikeCreateView, ShareListView, ShareCreateView, PostSearchView,
 )
 
@@ -20,7 +20,7 @@ urlpatterns = [
     path('shares/', ShareListView.as_view(), name='share-list'),
     path('shares/create/', ShareCreateView.as_view(), name='share-create'),
     path('posts/search/', PostSearchView.as_view(), name='post-search'),
-    path('connections/', ConnectionViewSet.as_view({'get': 'list', 'post': 'create'}), name='connection-list'),
-    path('connections/<int:pk>/', ConnectionViewSet.as_view({'get': 'retrieve'}), name='connection-detail'),
+    path('connections/', ConnectionListCreateView.as_view(), name='connection-list'),
+    path('connections/<int:pk>/', ConnectionListCreateView.as_view(), name='connection-detail'),
 ]
 
