5d9a429f5ef1e82bcefa30b11545c0f0f3dce969
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-05-22 23:04:25 +0600

    Update urls.py

--- social_media_task_of_eva/urls.py
+++ social_media_task_of_eva/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('App_auth.urls')),
+    path('api/main/', include('App_main.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
